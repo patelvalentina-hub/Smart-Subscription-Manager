468d2b98660a9683a918d6bc8feeff02a55ce999
Valentina Raynold Patel <patelvalentina@gmail.com>
2026-07-15 16:39:29 +0700

    Refactor templates into landing and dashboard pages

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder="app/templates", static_folder="app/static")
+app = Flask(
+    __name__, 
+    template_folder="app/templates", 
+    static_folder="app/static"
+    )
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
